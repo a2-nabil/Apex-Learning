daf809f9e85c148811dd89467e223c3621ecdd58
a2-nabil <mdmrnbl@gmail.com>
2024-06-26 15:58:39 +0600

    custom css added

---
+++ custom.js
@@ -0,0 +1,11 @@
+
+
+
+// Hero's text slider started
+$(".dis-slider").slick({
+    slidesToShow: 5,
+    slidestoScroll: 1,
+    prevArrow: '<button class="slick-arrow prev-arrow"> <i aria-hidden="true" class="fas fa-long-arrow-alt-left"></i> </button>',
+    nextArrow: '<button class="slick-arrow next-arrow"> <i aria-hidden="true" class="fas fa-long-arrow-alt-right"></i> </button>'
+});
+// Hero's text slider ended
